fb767735f16ad0a51f471269e32a2e8b52162e9a
Andrew Mackin <andrewkm@live.unc.edu>
2023-04-25 12:51:02 -0400

    Completed the code part of the questions. Next is the writeup.

--- hw6.R
+++ hw6.R
@@ -5,11 +5,12 @@ library(tidyverse)
 trees = read_csv('TS3_Raw_tree_data.csv')
 
 # Question 1
+
 # Creating new columns for the cities and states using a regular expression
 trees$just_city = str_match(trees$City, "^(.+),")[,2]
 trees$state = str_match(trees$City, ", ([:alpha:]{2})\\b")[,2]
 
-# Checking for N/A values
+# Checking for N/A values to confirm successful text parsing
 View(trees)
 is.na(trees$just_city)
 
@@ -21,11 +22,21 @@ ggplot(states, aes(x=state, y=record_num)) +
   geom_col()
 
 # Question 2
-# Filtering the trees by the records from North or South Carolina
 
+# Filtering the trees by the records from North or South Carolina
 carolina_trees = filter(trees, state == "NC" | state == "SC")
 # Grouping and summarizing by cities to count the number of cities
 carolina_cities = group_by(carolina_trees, just_city) %>%
   summarize()
 # Viewing the grouping to count the number of cities
 View(carolina_cities)
+
+# Question 3
+
+# Extracting a new column for the genus for the carolina_trees data
+carolina_trees$genus = str_match(carolina_trees$ScientificName, "^(.+) ")[,2]
+
+# Grouping by genus and summarizing by average canopy diameter before viewing
+avg_canopies = group_by(carolina_trees, genus) %>%
+  summarize(avg_canopy = mean(`AvgCdia (m)`))
+View(avg_canopies)
